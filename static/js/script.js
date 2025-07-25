5b33ec99977730d79230e59d3483e5e273393b37
Ehsan VK <gamer.good1400@gmail.com>
2025-07-25 19:09:11 +0300

    qrcode

---
+++ static/js/script.js
@@ -0,0 +1,64 @@
+document.getElementsByClassName(".custom-file-upload").addEventListener("drop", dropHandler);
+document
+  .getElementsByClassName("custom-file-upload")
+  .addEventListener("dragover", dragOverHandler);
+
+function dropHandler(ev) {
+  console.log("File(s) dropped");
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    //access the file
+    [...ev.dataTransfer.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === "file") {
+        const file = item.getAsFile();
+        console.log(`… file[${i}].name = ${file.name}`);
+        const formData = new FormData();
+        formData.append("file", file); // "file" is the name expected by the backend
+
+        fetch("http://192.168.1.113:5000/upload", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.text())
+        .then(result => {
+            console.log("Success:", result);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+        }
+        
+    });
+  } 
+  else {
+    [...ev.dataTransfer.files].forEach((file, i) => {
+      console.log(`… file[${i}].name = ${file.name}`);
+    });
+  }
+}
+
+document.getElementsByClassName("file-upload").addEventListener('change', () => {
+  console.log("eyvallah")
+  const file = document.getElementById("file-upload").files[0];
+  if (file) {
+    const formData = new FormData();
+    formData.append("file", files); 
+    fetch("http://192.168.1.113:5000/upload", {
+        method: "POST",
+        body: formData
+    })
+    
+    .then(response => response.text())
+    .then(result => {
+        console.log("Success:", result);
+    })
+    
+    .catch(error => {
+        console.error("Error:", error);
+    });
+    
+    }
+    
+});
